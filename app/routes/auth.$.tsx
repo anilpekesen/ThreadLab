b87ce71431aeb7105401126f88904182b4c17a0e
anil <2011010207111@ogrenci.karabuk.edu.tr>
2026-05-31 22:30:25 +0300

    Fix Shopify login route handler

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,7 +1,12 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticateShopify } from "~/shopify.server";
+import { authenticateShopify, login } from "~/shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  if (url.pathname === "/auth/login") {
+    return login(request);
+  }
+
   await authenticateShopify.admin(request);
   return null;
 };
